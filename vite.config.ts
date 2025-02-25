ba0f19ca20543e6a26b24107d3092443fc0bd4a9
Jenna Cioffi <jenna.cioffi@horrocks.com>
2025-02-25 12:56:43 -0700

    added to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-import { config } from "./src/utils/config";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import { config } from './src/utils/config';
 
 dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: config.routes_basename,
+  base: config.routes_basename, // This might need to just be replaced with [ process.env.VITE_BASE_URL || "/", ]
   build: {
-    target: "esnext",
+    target: 'esnext',
   },
   define: {
-    "process.env.NODE_ENV": JSON.stringify(
-      process.env.NODE_ENV || "development"
+    'process.env.NODE_ENV': JSON.stringify(
+      process.env.NODE_ENV || 'development'
     ),
   },
 });
