92c26823cfd85bfddb3add0d923bd6a56e6cd5cb
Jenna Cioffi <jenna.cioffi@horrocks.com>
2025-02-25 12:09:41 -0700

    updated config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  redirectUri_dev: "/",
-  redirectUri_prod: "/",
-  routes_basename: import.meta.env.BASE_URL || "/",
-  postLogoutRedirectUri: "/",
+  postLogoutRedirectUri: '/',
+  redirectUri_dev: '/',
+  redirectUri_prod: '/',
+  routes_basename: import.meta.env.BASE_URL || '/',
 };
